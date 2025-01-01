b16a8d8209448d08043ae1652718f480cac89299
mennareda568 <mennaghalwash12@gmail.com>
2025-01-01 19:14:48 +0200

    Add files via upload

---
+++ database/migrations/2024_12_31_165726_create_doctors_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('doc_image')->default("null");
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('department')->default("null");
+            $table->enum('gender',['female','male'])->default("male");
+            $table->string('phone')->default("null");
+            $table->string('address')->default("cairo");
+            $table->integer('age')->default("30");
+            $table->string('days')->default("null");
+            $table->string('time')->default("null");
+            $table->integer('Consultancyfees')->default("300");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
