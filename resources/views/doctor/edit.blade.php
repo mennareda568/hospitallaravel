17df7ad1b6cb0f8a1d0e775dd2227d2ac6073483
mennareda568 <mennaghalwash12@gmail.com>
2025-01-03 01:09:08 +0200

    Update edit.blade.php

--- resources/views/doctor/edit.blade.php
+++ resources/views/doctor/edit.blade.php
@@ -71,24 +71,6 @@
                  <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
 
-                <label>{{__('language.DAYS')}}</label>
-                <input type="text" name="days" class="form-control mb-4" value="{{$result->days}}">
-                @error('days')
-                 <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-
-                <label>{{__('language.TIME')}}</label>
-                <input type="text" name="time" class="form-control mb-4" value="{{$result->time}}">
-                @error('time')
-                 <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-
-                <label>{{__('language.CONSULTANCYFEES')}}</label>
-                <input type="text" name="Consultancyfees" class="form-control mb-4" value="{{$result->Consultancyfees}}">
-                @error('Consultancyfees')
-                 <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-
                 <input type="submit" value='{{__('language.UPDATEMYPROFILE')}}' class="form-control btn btn-success">
 
             </form>
